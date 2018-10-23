aef2433ef4212d028a5efedcd64de694daea3fa3
Liuqc2008 <313624981@qq.com>
2018-10-23 16:37:31 +0800

    weixinapi

---
+++ src/test/java/weixinapi/test/MenuTest.java
@@ -0,0 +1,39 @@
+package weixinapi.test;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+
+import weixin.sdk.api.MenuApi;
+
+public class MenuTest {
+	
+	@Ignore
+	@Test
+	public void CteateMenuTest() throws Exception {
+		Map<String, Object> param = new HashMap<String, Object>();
+		
+		String result = MenuApi.CreateMenu(param);
+		System.out.println(result);
+	}
+	
+	@Ignore
+	@Test
+	public void DeleteMenuTest() throws Exception {
+		String result = MenuApi.DeleteMenu();
+		System.out.println(result);
+	}
+	
+	@Ignore
+	@Test
+	public void GetMenuTest() throws Exception {
+		String result = MenuApi.GetMenu();
+		
+		System.out.println(result);
+	}
+}
